272ac946faab21b3370735fbb46ed0ceb8aa78b9
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-23 00:31:07 +0300

    feat: add FlavorTitle component with animated flavor section headings

---
+++ src/components/ui/flavor-title/flavor-title.component.jsx
@@ -0,0 +1,21 @@
+const FlavorTitle = () => {
+  return (
+    <div className="general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16">
+      <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
+        <h1>We here 6</h1>
+      </div>
+
+      <div style={{}} className="falvor-text-scroll">
+        <div className="bg-golden-mango pb-5 2xl:pt-0 pt-3 2xl:px-5 px-3">
+          <h2 className="text-warm-sunrise">freaking</h2>
+        </div>
+      </div>
+
+      <div className="overflow-hidden 2xl:py-0 py-3 second-text-split">
+        <h1>delicious flavors</h1>
+      </div>
+    </div>
+  );
+}
+
+export default FlavorTitle;
