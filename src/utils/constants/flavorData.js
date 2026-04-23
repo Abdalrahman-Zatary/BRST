ac245c3712028c86cd687b5d774773e247d89faf
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-23 10:06:44 +0300

    feat: add flavor data constants for home flavor sections

---
+++ src/utils/constants/flavorData.js
@@ -0,0 +1,34 @@
+const flavorLists = [
+  {
+    name: "Mango Blaz",
+    imageUrl: "mango-blaze",
+    rotation: "md:rotate-[-8deg] rotate-0",
+  },
+  {
+    name: "Berry Rush",
+    imageUrl: "berry-rush",
+    rotation: "md:rotate-[8deg] rotate-0",
+  },
+  {
+    name: "Wild Blueberry",
+    imageUrl: "wild-blueberry",
+    rotation: "md:rotate-[-8deg] rotate-0",
+  },
+  {
+    name: "Melon Drop",
+    imageUrl: "melon-drop",
+    rotation: "md:rotate-[8deg] rotate-0",
+  },
+  {
+    name: "Coconut Cloud",
+    imageUrl: "coconut-cloud",
+    rotation: "md:rotate-[-8deg] rotate-0",
+  },
+  {
+    name: "Lemon Zap",
+    imageUrl: "lemon-zap",
+    rotation: "md:rotate-[8deg] rotate-0",
+  },
+];
+
+export { flavorLists };
