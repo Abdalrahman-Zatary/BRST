12540879e55957ea9f09fba558ebbca104e4c3c6
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-24 03:14:11 +0300

    feat: add scroll-triggered animation and fixed CTA in FlavorSection

--- src/components/layout/flavor-section/flavor-section.component.jsx
+++ src/components/layout/flavor-section/flavor-section.component.jsx
@@ -1,16 +1,90 @@
 import FlavorSlider from '../../ui/flavor-slider/flavor-slider.component';
 import FlavorTitle from '../../ui/flavor-title/flavor-title.component';
+import { useGSAP } from '@gsap/react';
+import { useMediaQuery } from 'react-responsive';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 const FlavorSection = () => {
-  return(
-    <section className="flavor-section">
-      <div className="h-full flex lg:flex-row flex-col items-center relative">
-        <div className="lg:w-[57%] flex-none h-80 lg:h-full xl:mt-0 md:mt-20 mt-0">
+  const isTablet = useMediaQuery({
+    query: "(min-width: 1023px)"
+  });
+
+  useGSAP(() => {
+    if (isTablet) return;
+
+    gsap.fromTo(
+      ".cta-wrapper",
+      {
+        opacity: 0,
+        y: 50,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: ".flavor-section",
+          start: "top 70%",
+          end: "bottom bottom",
+          toggleActions: "play reverse play reverse",
+        },
+      },
+    );
+    
+    ScrollTrigger.create({
+      trigger: "flavor-section",
+      start: "top top",
+      end: "bottom bottom",
+
+      onEnter: () => {
+        gsap.set(".cta-wrapper", {
+          position: "fixed",
+          bottom: "0",
+          zIndex: "999",
+        });
+      },
+
+      onLeave: () => {
+        gsap.set(".cta-wrapper", {
+          position: "relative",
+          bottom: "0",
+          left: "0",
+        });
+      },
+      onEnterBack: () => {
+        gsap.set(".cta-wrapper", {
+          position: "fixed",
+          bottom: "0",
+        });
+      },
+      onLeaveBack: () => {
+        gsap.set(".cta-wrapper", {
+          position: "relative",
+          bottom: "0",
+          left: "0",
+        });
+      },
+    });
+  }, [isTablet]);
+
+  return (
+    <section className="flavor-section pt-16">
+      {/* lg:h-dvh */}
+      <div className="relative flex lg:flex-row flex-col w-full slider-elements-container">
+        {/* lg:w-[57%] lg:h-full lg:flex-none flex items-center justify-center lg:py-0 py-16 */}
+        <div className="lg:w-[57%] lg:h-full lg:flex-none flex items-center justify-center mt-10">
           <FlavorTitle />
         </div>
-        <div className="h-full">
+        {/* overflow-hidden */}
+        <div className="lg:h-full">
           <FlavorSlider />
         </div>
+      </div>
+      {/* absolute left-0 w-full bg-transparent */}
+      <div className="cta-wrapper absolute h-32 w-full flex justify-center items-center lg:bg-transparent bg-warm-sunrise">
+        <a className="cta-btn text-deep-navy bg-mango-burst uppercase font-bold text-sm rounded-full md:p-3.5 p-3 md:px-12 px-10 cursor-pointer">
+          Chug a SPYLT
+        </a>
       </div>
     </section>
   );
