72c86bc2090e5e6ea718373e42037e4d80582211
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-26 23:17:15 +0300

    feat: Animate location section text and apply scroll-triggered effects

--- src/components/layout/location-section/location-section.component.jsx
+++ src/components/layout/location-section/location-section.component.jsx
@@ -1,4 +1,52 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { SplitText } from 'gsap/all';
+
 const LocationSection = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".location-title", {
+      type: "chars",
+    });
+    const paragraphSpilt = SplitText.create(".location-section p", {
+      type: "words, lines",
+      linesClass: "paragraph-line",
+    });
+
+    const contentTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".location-section",
+        start: "30% center",
+      },
+    });
+
+    contentTl
+      .from(titleSplit.chars, {
+        yPercent: 100,
+        stagger: 0.02,
+        ease: "power2.out",
+      })
+      .from(paragraphSpilt.words, {
+        duration: 1,
+        stagger: 0.01,
+        yPercent: 300,
+        rotate: 3,
+        ease: "power1.inOut",
+      });
+
+    const titleTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".location-section",
+        start: "40% center",
+      },
+    });
+    titleTl.to(".location-text-scroll", {
+      duration: 1,
+      opacity: 1,
+      clipPath: "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)",
+      ease: "power1.inOut",
+    });
+  });
+
   return (
     <section className="location-section">
       <img
@@ -15,15 +63,15 @@ const LocationSection = () => {
         <div className="location-content">
           <div className="general-title flex flex-col items-start">
             <div className="overflow-hidden lg:py-0 py-3">
-              <h1 className="text-warm-sunrise">Right around</h1>
+              <h1 className="location-title text-warm-sunrise">Right around</h1>
             </div>
             <div
               style={
                 {
-                  // clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
+                  clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
                 }
               }
-              className="rotate-3 -mt-1 text-nowrap opacity-100"
+              className="location-text-scroll rotate-3 -mt-1 text-nowrap opacity-0"
             >
               <div className="bg-mango-burst lg:pt-0 pt-2 lg:pb-2 pb-4 px-3">
                 <h2 className="text-warm-sunrise">the corner</h2>
