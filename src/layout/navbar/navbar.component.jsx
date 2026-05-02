62a077757fb7be11ca8150537ee6594e9209bce7
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-02 19:43:44 +0300

    feat: wrap Logo in Link for navigation in Navbar component

--- src/layout/navbar/navbar.component.jsx
+++ src/layout/navbar/navbar.component.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 import MenubarButton from '../../components/ui/menubar-button/menubar-button.component';
 import Logo from '../../components/common/logo/logo.component';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 flex justify-between items-center mx-5 z-50 md:h-22.5 bg-transparent">
-      <Logo />
+    <nav className="fixed z-120 top-0 left-0 right-0 flex justify-between items-center mx-5 md:h-22.5 bg-transparent">
+      <Link to="/">
+        <Logo />
+      </Link>
       <MenubarButton />
     </nav>
   );
