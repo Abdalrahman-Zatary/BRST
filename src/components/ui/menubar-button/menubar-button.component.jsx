2ae52120b2516df093c307219b0eb4a97b440f7c
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-03 15:18:55 +0300

    refactor: animate and style menu toggle button with enhanced motion and ref management

--- src/components/ui/menubar-button/menubar-button.component.jsx
+++ src/components/ui/menubar-button/menubar-button.component.jsx
@@ -7,6 +7,7 @@ import { MenubarContext } from '../../../context/menubar/menubar.contaxt';
 const MenubarButton = () => {
   const { isOpenMenubar, setIsOpenMenubar } = useContext(MenubarContext);
 
+  const buttonRef = useRef(null);
   const line1Ref = useRef(null);
   const line2Ref = useRef(null);
   const tlRef = useRef(null);
@@ -17,38 +18,54 @@ const MenubarButton = () => {
       paused: true,
     });
 
-    tlRef.current.to(
-      line1Ref.current,
-      {
-        y: 5.5,
-        duration: 0.18,
-        ease: "power2.inOut",
-      },
-      0,
-    )
+    tlRef.current
+      .to(
+        line1Ref.current,
+        {
+          y: 5.5,
+          duration: 0.36,
+          ease: "power2.inOut",
+        },
+        0,
+      )
       .to(
         line2Ref.current,
         {
           y: -5.5,
-          duration: 0.18,
+          duration: 0.36,
           ease: "power2.inOut",
         },
         0,
       )
       .to(line1Ref.current, {
         rotate: 45,
-        duration: 0.22,
+        duration: 0.44,
         ease: "power3.inOut",
       })
       .to(
         line2Ref.current,
         {
           rotate: -45,
-          duration: 0.22,
+          duration: 0.44,
           ease: "power3.inOut",
         },
         "<",
-      );
+      )
+      .to(line1Ref.current, {
+        width: 22,
+        duration: 0.44,
+        ease: "power3.inOut",
+      })
+      .to(line2Ref.current, {
+        width: 22,
+        duration: 0.44,
+        ease: "power3.inOut",
+      }, "<")
+      .to(buttonRef.current, {
+        backgroundColor: "#ffffff",
+        duration: 0.88,
+        ease: "power1.out",
+      }, "<");
   }, []);
 
   useEffect(() => {
@@ -62,12 +79,19 @@ const MenubarButton = () => {
 
   return (
     <button
+      ref={buttonRef}
       onClick={toggelIsOpenMenubar}
       aria-label={isOpenMenubar ? "Close menubar" : "Open menubar"}
-      className="relative z-1000 flex flex-col justify-center items-end gap-2 cursor-pointer p-2"
+      className="relative z-1000 flex flex-col justify-center items-center w-10 h-10 gap-2 cursor-pointer rounded-full bg-transparent p-2"
     >
-      <span ref={line1Ref} className="block w-11 h-0.5 bg-deep-navy rounded-xs" />
-      <span ref={line2Ref} className="block w-11 h-0.5 bg-deep-navy rounded-xs" />
+      <span
+        ref={line1Ref}
+        className="block w-10 h-0.5 bg-deep-navy rounded-xs"
+      />
+      <span
+        ref={line2Ref}
+        className="block w-10 h-0.5 bg-deep-navy rounded-xs"
+      />
     </button>
   );
 };
