9e6a3e2b5567f4dc30d758e2419075e51edb186d
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-07 00:55:10 +0300

    feat: add TestimonialSection and location image to Programs page

--- src/pages/programs/programs.component.jsx
+++ src/pages/programs/programs.component.jsx
@@ -1,11 +1,26 @@
-import { ProgramsHeroSection, ProgramsMessageSection, ProgramsEthosSection } from '../../sections/sections';
+import {
+  ProgramsHeroSection,
+  ProgramsMessageSection,
+  ProgramsEthosSection,
+  TestimonialSection,
+} from '../../sections/sections';
 
 const Programs = () => {
   return (
     <>
       <ProgramsHeroSection />
       <ProgramsMessageSection />
-      <ProgramsEthosSection />
+      <div>
+        <ProgramsEthosSection />
+        <TestimonialSection withOffset={true} />
+      </div>
+      <div className="bg-near-black">
+        <img
+          src="https://i.ibb.co/cm31PH9/location-dip.webp"
+          alt="dip"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </>
   );
 };
