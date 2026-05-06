f3c3f4f27abbba5667d4c0ec93eade73411cfbef
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-06 11:08:23 +0300

    fix: update messages in ProgramsMessageSection component

--- src/sections/programs-message-section/programs-message-section.component.jsx
+++ src/sections/programs-message-section/programs-message-section.component.jsx
@@ -9,13 +9,13 @@ const ProgramsMessageSection = () => {
             <h1 className="programs-first-message">
               Back in the day, you'd catch us at the skatepark, football field,
               or bombing down slopes—we just couldn't sit still. And one thing
-              was guaranteed: we were chugging chocolate milk.
+              was guaranteed: we were chugging Mango Blaze.
             </h1>
           </div>
           <div className="programs-question">
             <h2>
               And you know what? <br />
-              We're always ready to throw back some SPYLT Milk!
+              We're always ready to throw back some BRST Juice!
             </h2>
           </div>
         </div>
