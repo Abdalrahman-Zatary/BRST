df54b6984743010f4003437718a359ccf7494fcb
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-07 23:45:28 +0300

    feat: add location image section to About Us page

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -5,6 +5,13 @@ const About = () => {
     <>
       <AboutHeroSection />
       <AboutEthosSection />
+      <div className="bg-near-black">
+        <img
+          src="https://i.ibb.co/cm31PH9/location-dip.webp"
+          alt="dip"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </>
   );
 };
