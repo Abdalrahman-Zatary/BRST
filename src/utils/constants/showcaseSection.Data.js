9b581e14ceab8667cf7cb16125d0c921acb37c57
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-12 13:23:07 +0300

    feat: add showcase section data for Promo and Origins sections

---
+++ src/utils/constants/showcaseSection.Data.js
@@ -0,0 +1,24 @@
+export const showcaseSectionData = {
+  Promo: {
+    bgImageUrl: "https://i.ibb.co/hRbpQGgq/stores-find.webp",
+    title: "Can’t find BRST",
+    subtitleImageUrl: "https://i.ibb.co/ZpMT5Q3z/stores-sub-title.webp",
+    description: `BRST is now available at thousands of grocery stores around the country, 
+                  but there are thousands more that we’d love to be in.`,
+    buttonText: "Contact us",
+    buttonLayout: "backdrop-blur-xl border border-warm-sunrise/40 hover:bg-warm-sunrise text-warm-sunrise hover:text-deep-navy transition-all duration-500",
+    imgdepth: true,
+  },
+  origins: {
+    bgImageUrl: "https://i.ibb.co/KxFPt08L/menubar-3.webp",
+    title: "Live Loud and",
+    subtitleImageUrl: "https://i.ibb.co/hFr3LJnz/leran.webp",
+    description: `Being a Brand Rebel will not only push you personally, but will also give 
+                  you the chance to fuel your best life! This journey offers the opportunity 
+                  to maximize your energy and develop bold Marketing skills while building 
+                  real connections for your future.`,
+    buttonText: "Contact us",
+    buttonLayout: "bg-mango-burst text-deep-navy",
+    imgdepth: false,
+  },
+};
