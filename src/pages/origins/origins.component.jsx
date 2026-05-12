dfbcdeb6d9743880944472e00ea47c9c641f2264
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-12 13:58:25 +0300

    feat: add TestimonialSection and background image to Origins page

--- src/pages/origins/origins.component.jsx
+++ src/pages/origins/origins.component.jsx
@@ -3,6 +3,7 @@ import {
   OriginsCarouselSection,
   FlavorSection,
   ShowcaseSection,
+  TestimonialSection,
 } from '../../sections/sections';
 
 import { showcaseSectionData } from '../../utils/constants/showcaseSection.Data';
@@ -20,6 +21,14 @@ const Origins = () => {
         argement={false}
       />
       <ShowcaseSection {...origins} />
+      <TestimonialSection withOffset={false} />
+      <div className="bg-near-black">
+        <img
+          src="https://i.ibb.co/cm31PH9/location-dip.webp"
+          alt="dip"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </>
   );
 };
