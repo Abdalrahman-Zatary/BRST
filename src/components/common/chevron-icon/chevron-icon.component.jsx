03806b92b31ea2fe764fa02e99842f374efd573f
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-05-13 17:19:17 +0300

    feat: add ChevronIcon component for use in contact page UI elements

---
+++ src/components/common/chevron-icon/chevron-icon.component.jsx
@@ -0,0 +1,13 @@
+const ChevronIcon = () => (
+  <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
+    <path
+      d="M4 11.5L9 6.5L14 11.5"
+      stroke="currentColor"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default ChevronIcon;
